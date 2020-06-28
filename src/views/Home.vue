507d83c2a329cf01ab4bf1213537b1eafe826791
Luka Salevic <salevic@lu-ka.me>
2020-06-28 02:22:32 +0200

    Basic Index page style to be refined Refactor grids by removing 0-size columns and rows

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,28 @@
 <template>
     <div class="wrapper-div index-wrapper">
-        <router-link to="/projects">Projects</router-link>
+        <div class="index-box box-top-left black-out">
+            <div class="box-letter letter-l">L</div>
+            <router-link class="box-link white-out" to="/about"
+                >ABOUT</router-link
+            >
+        </div>
+        <div class="index-box box-top-right white-out">
+            <div class="box-letter letter-u">U</div>
+            <router-link class="box-link black-out" to="/projects"
+                >PROJECTS</router-link
+            >
+        </div>
+        <div class="index-box box-bottom-left white-out">
+            <div class="box-letter letter-k">K</div>
+            <router-link class="box-link black-out" to="/contact"
+                >CONTACT</router-link
+            >
+        </div>
+        <div class="index-box box-bottom-right black-out">
+            <div class="box-letter letter-a">A</div>
+            <router-link class="box-link white-out" to="/rambling"
+                >RAMBLING</router-link
+            >
         </div>
     </div>
 </template>
@@ -13,3 +35,109 @@
         }
     };
 </script>
+
+<style lang="scss">
+    a {
+        text-decoration: none;
+        padding: 0;
+    }
+
+    .index-wrapper {
+        height: 100%;
+        display: grid;
+        grid-template: 1fr 1fr / 1fr 1fr;
+    }
+
+    .white-out {
+        background: $secondary-color;
+        color: $main-color;
+    }
+    .black-out {
+        background: $main-color;
+        color: $secondary-color;
+    }
+
+    .box-letter {
+        font-size: 16vh;
+        align-self: end;
+    }
+
+    .index-box {
+        display: grid;
+
+        .box-link {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
+        }
+    }
+
+    .box-top-left {
+        grid-template-columns: 3fr 1fr 110px;
+        grid-template-rows: 1fr 1fr 110px;
+        grid-template-areas:
+            ".       .       .  "
+            ".    letterL    .  "
+            ".       .     linkL";
+
+        .letter-l {
+            grid-area: letterL;
+            justify-self: flex-end;
+        }
+
+        .box-link {
+            grid-area: linkL;
+        }
+    }
+
+    .box-top-right {
+        grid-template-columns: 110px 1fr 3fr;
+        grid-template-rows: 1fr 1fr 110px;
+        grid-template-areas:
+            "  .      .    ."
+            "  .   letterU ."
+            "linkU    .    .";
+
+        .letter-u {
+            grid-area: letterU;
+        }
+
+        .box-link {
+            grid-area: linkU;
+        }
+    }
+
+    .box-bottom-left {
+        grid-template-columns: 3fr 1fr 110px;
+        grid-template-rows: 110px 1fr 1fr;
+        grid-template-areas:
+            ".      .     linkK"
+            ".   letterK    .  "
+            ".      .       .  ";
+
+        .letter-k {
+            grid-area: letterK;
+            justify-self: flex-end;
+        }
+        .box-link {
+            grid-area: linkK;
+        }
+    }
+
+    .box-bottom-right {
+        grid-template-columns: 110px 1fr 3fr;
+        grid-template-rows: 110px 1fr 1fr;
+        grid-template-areas:
+            "linkA    .       ."
+            "  .   letterA    ."
+            "  .      .       .";
+
+        .letter-a {
+            grid-area: letterA;
+        }
+        .box-link {
+            grid-area: linkA;
+        }
+    }
+</style>
