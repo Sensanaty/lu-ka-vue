6125de21f85ff00d746202f53c01aaa92dee714b
Luka Salevic <salevic@lu-ka.me>
2020-07-03 00:39:17 +0200

    Create ProjectCard component

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="project-card">
+        <h1 class="project-title">{{ textTitle }}</h1>
+        <h2 class="project-subtitle">{{ textSubtitle }}</h2>
+        <p class="project-description" v-for="line in textDescription" :key="line">{{ line }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ProjectCard",
+        props: {
+            textTitle: String,
+            textSubtitle: String,
+            textDescription: Array
+        }
+    };
+</script>
+
+<style lang="scss">
+    .project-card {
+        background: lighten($main-color, 5);
+        margin: 2% 0;
+        display: flex;
+        flex-flow: column nowrap;
+        justify-content: center;
+        width: 55%;
+        padding: 15px 2%;
+        box-shadow: -6px 6px 0px 0px darken($main-color, 5);
+
+        h2:after {
+            color: $secondary-color;
+            content: " ";
+            display: block;
+            border: 3px solid $highlight-color;
+            margin: 0 -2%;
+        }
+
+        h1 {
+            font-size: 2.3em;
+            margin-bottom: 0;
+        }
+
+        p {
+            color: $secondary-color;
+            font-weight: 400;
+            font-family: "Roboto", sans-serif;
+            line-height: 1.467;
+            text-rendering: optimizelegibility !important;
+            letter-spacing: 0.03em;
+            font-size: 1.13em;
+        }
+    }
+</style>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,18 +1,51 @@
 <template>
     <div class="wrapper-div projects-wrapper">
         <NavBar />
-        <div>Projects</div>
+        <div class="project-card-wrapper">
+            <ProjectCard
+                text-title="Spotlight"
+                text-subtitle="Keep track of your restaurant's online presence in one convenient spot"
+                :text-description="spotlight"
+            />
+            <ProjectCard
+                text-title="AirP2P"
+                text-subtitle="Rent out those games gathering dust and earn a quick buck"
+                :text-description="airp2p"
+            />
+            <ProjectCard
+                text-title="Izzicup"
+                text-subtitle="idk something about izzicup.com"
+                :text-description="izzicup"
+            />
+            <ProjectCard
+                text-title="Steven de Graaf"
+                text-subtitle="steven de graaf is a website blah blah blah"
+                :text-description="steven"
+            />
+            <ProjectCard
+                text-title="How's the Cash?"
+                text-subtitle="uhh hello dolla dolla bills yall GET THAT MONEY C.R.E.A.M."
+                :text-description="cash"
+            />
+            <ProjectCard
+                text-title="LU-KA.ME"
+                text-subtitle="oh wow look it's my own website lol what a project"
+                :text-description="luka"
+            />
+        </div>
     </div>
 </template>
 
 <script>
     import NavBar from "@/components/NavBar";
+    import ProjectCard from "@/components/ProjectCard";
     export default {
         name: "Projects",
         metaInfo: {
             title: "L U K A | Projects"
         },
         components: {
+            ProjectCard,
             NavBar
         }
     };
