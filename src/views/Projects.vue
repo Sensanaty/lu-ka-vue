5b8697f04c23d001fa4c2ef336a7d41389fc5957
Luka Salevic <salevic@lu-ka.me>
2020-07-03 00:39:41 +0200

    Add all the text files and have them linked to the project cards

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -39,14 +39,50 @@
 <script>
     import NavBar from "@/components/NavBar";
     import ProjectCard from "@/components/ProjectCard";
+    import Spotlight from "@/assets/texts/spotlight.txt";
+    import Airp2p from "@/assets/texts/airp2p.txt";
+    import Izzicup from "@/assets/texts/izzicup.txt";
+    import Steven from "@/assets/texts/steven.txt";
+    import Cash from "@/assets/texts/cash.txt";
+    import Luka from "@/assets/texts/luka.txt";
+
     export default {
         name: "Projects",
         metaInfo: {
-            title: "L U K A | Projects"
+            title: "L U K A | PROJECTS"
         },
         components: {
             ProjectCard,
             NavBar
+        },
+        data: function() {
+            return {
+                spotlight: Spotlight.split("\n\n"),
+                airp2p: Airp2p.split("\n\n"),
+                izzicup: Izzicup.split("\n\n"),
+                steven: Steven.split("\n\n"),
+                cash: Cash.split("\n\n"),
+                luka: Luka.split("\n\n")
+            };
         }
     };
 </script>
+
+<style lang="scss">
+    ::-webkit-scrollbar {
+        display: none;
+    }
+
+    .projects-wrapper {
+        height: 100%;
+        display: grid;
+        grid-template: 0.2fr 3fr / 1fr;
+    }
+
+    .project-card-wrapper {
+        margin-top: 12px;
+        display: flex;
+        align-items: center;
+        flex-flow: column nowrap;
+    }
+</style>
