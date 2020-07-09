2a2f1fdd1818e82decd21453b1e066389e60ff74
Luka Salevic <salevic@lu-ka.me>
2020-07-09 18:19:57 +0200

    Finish basic design and functionality of Rambles page

--- src/views/Rambles.vue
+++ src/views/Rambles.vue
@@ -1,13 +1,47 @@
 <template>
     <div class="wrapper-div rambling-wrapper">
-        <div>Ramblings</div>
+        <div class="ramble-cards-wrapper">
+            <RambleCard
+                v-for="route in rambleList"
+                :key="route.title"
+                :title="route.title"
+                :subtitle="route.subtitle"
+                :ramble-url="route.url"
+            />
+        </div>
     </div>
 </template>
 
 <script>
+    import RambleCard from "@/components/RambleCard";
+    import rambleList from "@/assets/rambles/rambleList.json";
+
     export default {
         metaInfo: {
-            title: "L U K A | Ramblings"
+            title: "L U K A | RAMBLES"
+        },
+        components: {
+            RambleCard
+        },
+        data() {
+            return {
+                rambleList
+            };
         }
     };
 </script>
+
+<style lang="scss">
+    .rambling-wrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 1.5%;
+    }
+
+    .ramble-cards-wrapper {
+        display: flex;
+        flex-flow: column nowrap;
+        width: 60%;
+    }
+</style>
